f91b28b955a2cfc5ca40c036569ad84b0c78b69a
Richard5656 <80961838+Richard5656@users.noreply.github.com>
2022-01-01 14:18:30 -0800

    This is the progress on the items, equipment, and inventory
    I havent set any of the functions yet Im planning to add more functionality later.

---
+++ Rthon/Data/inventory_equipment_items_etc.py
@@ -0,0 +1,51 @@
+inventory = []
+main_slot = []
+equipment = []
+missions = []
+buyable = []
+
+
+def stone_sword_usage():
+	pass
+
+def stone_knife_usage():
+	pass
+
+
+def flashlight_usage():
+    pass
+
+
+def copper_pickaxe_usage():
+    pass
+
+def backpack_10x10_usage():
+    pass
+
+stone_sword = {
+	"attack": 10,
+	"usage": stone_sword_usage
+}
+
+
+stone_sword = {
+	"attack": 5,
+	"usage": stone_knife_usage
+}
+
+stone_chest_plate ={
+	"defense": 4
+}
+
+flashlight ={
+    "usage": flashlight_usage
+}
+
+copper_pickaxe = {
+    "usage": copper_pickaxe_usage
+}
+
+backpack_10x10 = {
+    "usage": backpack_10x10_usage
+
+}
